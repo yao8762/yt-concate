ace8099c412a34f1b4322d7aa07715a450f1d4d4
yao8762 <hot665992002@gmail.com>
2022-04-29 16:21:13 +0800

    Successfully download captions and save video list to file

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,7 +1,10 @@
 from yt_concate.pipeline.steps.get_video_list import GetVideoList
+from yt_concate.pipeline.steps.download_captions import DownloadCaptions
+from yt_concate.pipeline.steps.prelight import Preflight
+from yt_concate.pipeline.steps.postlight import Postflight
 from yt_concate.pipeline.steps.step import StepException
-
 from yt_concate.pipeline.pipeline import Pipeline
+from yt_concate.utils import Utils
 
 
 CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
@@ -10,14 +13,18 @@ CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
 def main():
     inputs = {
         'channel_id': CHANNEL_ID
+
     }
     steps = [
+        Preflight(),
+        Postflight(),
         GetVideoList(),
-
+        DownloadCaptions(),
         ]
 
+    utils = Utils()
     p = Pipeline(steps)
-    p.run(inputs)
+    p.run(inputs, utils)
 
 
 if __name__ == '__main__':
---
+++ yt_concate/pipeline/steps/postlight.py
@@ -0,0 +1,7 @@
+from .step import Step
+
+
+class Postflight(Step):
+    def process(self, data, inputs, utils):
+        print('in Postflight')
+
