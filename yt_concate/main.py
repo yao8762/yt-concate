1cb90584d1ac53280ed0409dcd0de5e23bbe3a1c
yao8762 <hot665992002@gmail.com>
2022-05-03 14:38:57 +0800

    finished downloading videos

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,7 +1,10 @@
 from yt_concate.pipeline.steps.prelight import Preflight
 from yt_concate.pipeline.steps.get_video_list import GetVideoList
+from yt_concate.pipeline.steps.initialize_yt import InitializeYT
 from yt_concate.pipeline.steps.download_captions import DownloadCaptions
 from yt_concate.pipeline.steps.read_caption import ReadCaption
+from yt_concate.pipeline.steps.search import Search
+from yt_concate.pipeline.steps.download_videos import DownloadVideos
 from yt_concate.pipeline.steps.postlight import Postflight
 from yt_concate.pipeline.steps.step import StepException
 from yt_concate.pipeline.pipeline import Pipeline
@@ -13,14 +16,17 @@ CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
 
 def main():
     inputs = {
-        'channel_id': CHANNEL_ID
-
+        'channel_id': CHANNEL_ID,
+        'search_word': 'incredible',
     }
     steps = [
         Preflight(),
         GetVideoList(),
+        InitializeYT(),
         DownloadCaptions(),
         ReadCaption(),
+        Search(),
+        DownloadVideos(),
         Postflight(),
     ]
 
